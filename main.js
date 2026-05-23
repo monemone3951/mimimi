4b4a538009b6b8ba02880f89a39ca66536f3e384
monemone3951 <tsubasanao254@gmail.com>
2026-05-23 16:02:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // main.js
-// GitHubの中にあるすべてのファイル（HTML/CSS/JS）をそのままブラウザに届けるプログラム
-
 import { serveDir } from "jsr:@std/http/file-server";
 
 Deno.serve((req) => {
   return serveDir(req, {
-    fsRoot: ".",          // リポジトリのルートフォルダを対象にする
-    showDirListing: false, // フォルダのファイル一覧は見せない
+    // index.html が入っているフォルダを直接指定します
+    fsRoot: "./tmp/build/src", 
+    showDirListing: false,
   });
 });
